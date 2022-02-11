f599d09a4050d90cbdd74ebf01f0666026cb0fb2
Sohan Subhash <sohansubhash@users.noreply.github.com>
2022-02-10 18:27:28 -0800

    new authorship

--- species/models.py
+++ species/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Species(models.Model):
-  
+    dex_id = models.IntegerField()
+    name = models.CharField(max_length=30)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    color_id = models.IntegerField()
+
+
